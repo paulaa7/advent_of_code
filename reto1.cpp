6d16ec90bf818fea3cead2da2079cfed604361b0
paulaa7 <145342191+paulaa7@users.noreply.github.com>
2025-12-01 11:37:46 +0100

    Create reto1.cpp

---
+++ reto1.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+
+int main(int argc, char *argv[]) {
+    ifstream archivo("input");
+    int sol = 0;
+    int posicion = 50;
+    int valor, div, v;
+
+    if (archivo.is_open()) {
+        string linea;
+
+        while (getline(archivo, linea)) {
+            char tipo = linea[0];
+            int valor = stoi(linea.substr(1));
+            
+            if (tipo == 'R') {
+                for (int i=0; i<valor; i++){
+                    posicion += 1;
+                    if (posicion == 0) sol ++;
+                    else {
+                        posicion %= 100;
+                        if (posicion==0) sol ++;
+                    }
+                }
+            }
+            else {
+                for (int i=0; i<valor; i++){
+                    posicion -= 1;
+                    if (posicion == 0) sol ++;
+                    else {
+                        posicion %= 100;
+                        if (posicion == 0) sol ++;
+                    }
+                }
+            }
+        }
+        archivo.close();
+        cout << "solución: " << sol << endl;
+    } else {
+        cout << "no se pudo abrir el archivo." << endl;
+    }
+    return 0;
+}
